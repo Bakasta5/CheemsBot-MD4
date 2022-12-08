bb6284e26111965a9b445777a4cfebc208a805be
Bakasta5 <120108258+Bakasta5@users.noreply.github.com>
2022-12-08 16:09:42 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,7 +18,7 @@
 //i give permission to do as long as you
 //put Xeons youtube channel link in the video
 //description and tag me on githuh repo, 
-//thank you🙂💗
+//thank you🙂💗💗
 //════════════════════════════//
 
 const fs = require('fs')
